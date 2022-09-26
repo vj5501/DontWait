125502ed4a130b4e58e0f13326db89a44abe3c11
Vishnu Jadhav <78802162+vj5501@users.noreply.github.com>
2022-09-26 18:40:17 +0530

    ThirdCommit
    Connected Firebase Cloud Database with the application and
    Added Data from our app to Cloud Database
    In Android studio we linked firebase data base using dependancies and google firebase console

--- app/src/main/java/com/vj/dontwaitapp/OfficerHome.java
+++ app/src/main/java/com/vj/dontwaitapp/OfficerHome.java
@@ -1,20 +1,65 @@
 package com.vj.dontwaitapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class OfficerHome extends AppCompatActivity {
 
+    EditText etSource,etDesti, etBusno;
+    EditText etTime;
+    FirebaseFirestore firestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_officer_home);
+        etSource=findViewById(R.id.etSource);
+        etDesti=findViewById(R.id.etDest);
+        etBusno=findViewById(R.id.etbusn);
+        etTime=findViewById(R.id.etTime);
+        etTime.setText(Timestamp.now().toDate().toString());
+
+        firestore = FirebaseFirestore.getInstance ( ) ;
+
     }
 
     public void addEntry(View view) {
-        Toast.makeText(this, "Added SuccessFully", Toast.LENGTH_SHORT).show();
+        String busno= etBusno.getText().toString();
+        String source= etSource.getText().toString();
+        String destination= etDesti.getText().toString();
+        String time= (Timestamp.now().toDate().toString());
+
+        Map<String,Object > route = new HashMap<>();
+        route.put ( " Bus no. " , busno ) ;
+        route.put ( " Source " , source ) ;
+        route.put ( " Destination " , destination ) ;
+        route.put ( " Leaving Time " , time) ;
+
+        firestore.collection("Routes").add(route).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(OfficerHome.this, "Successfully Added Route to Database", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(OfficerHome.this, "Error:Cannot Route to Database", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
